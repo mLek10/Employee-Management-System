e1c2251f1b6c15296f25753c351b15ba204970b2
Emily Caton <caton.emily@yahoo.com>
2023-09-13 12:17:53 -0500

    wrote index.js, troubleshoot

--- index.js
+++ index.js
@@ -1,2 +1,129 @@
 // Import and require mysql2
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+
+const inquirer = require('inquirer');
+const mysql = require('mysql2/promise');
+
+// Create a connection to the MySQL database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'employee_db',
+});
+
+// Function to view all departments
+async function viewDepartments() {
+  const [rows] = await connection.query('SELECT * FROM department');
+  console.table(rows);
+  mainMenu();
+}
+
+// Function to view all roles
+async function viewRoles() {
+  const [rows] = await connection.query('SELECT * FROM role');
+  console.table(rows);
+  mainMenu();
+}
+
+// Function to view all employees
+async function viewEmployees() {
+  const [rows] = await connection.query('SELECT * FROM employee');
+  console.table(rows);
+  mainMenu();
+}
+
+// Function to add a department
+async function addDepartment() {
+  const { name } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter the name of the department:',
+    },
+  ]);
+  
+  await connection.query('INSERT INTO department (name) VALUES (?)', [name]);
+  console.log(`Department "${name}" added successfully.`);
+  mainMenu();
+}
+
+// Function to add a role
+async function addRole() {
+  const departments = await connection.query('SELECT * FROM department');
+  const departmentChoices = departments[0].map((department) => ({
+    name: department.name,
+    value: department.id,
+  }));
+
+  const { title, salary, departmentId } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of the role:',
+    },
+    {
+      type: 'number',
+      name: 'salary',
+      message: 'Enter the salary for the role:',
+    },
+    {
+      type: 'list',
+      name: 'departmentId',
+      message: 'Select the department for the role:',
+      choices: departmentChoices,
+    },
+  ]);
+
+  await connection.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [
+    title,
+    salary,
+    departmentId,
+  ]);
+  console.log(`Role "${title}" added successfully.`);
+  mainMenu();
+}
+
+// Function to start the application
+async function mainMenu() {
+  const { action } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'Select an action:',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Quit',
+      ],
+    },
+  ]);
+
+  switch (action) {
+    case 'View all departments':
+      viewDepartments();
+      break;
+    case 'View all roles':
+      viewRoles();
+      break;
+    case 'View all employees':
+      viewEmployees();
+      break;
+    case 'Add a department':
+      addDepartment();
+      break;
+    case 'Add a role':
+      addRole();
+      break;
+    case 'Quit':
+      connection.end();
+      console.log('Goodbye!');
+      break;
+  }
+}
+
+// Start the application
+mainMenu();
